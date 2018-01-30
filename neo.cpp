3a77fc3ed5c2d0f531a2f4231251ab5dabe31ac0
Rishabh Singh <Ace@upes-500061070.DDN.UPES.AC.IN>
2018-01-30 15:25:58 +0530

    first commit

---
+++ neo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	cout<<"This is NEO";
+
+	return 0;
+}
+
---
+++ qaz/file1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main()
+{cout<<"this is file1 ";
+
+return 0;}
+
+
+
